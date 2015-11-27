f53e0eea8f610158f8f3f32cbb089ba30c1a82e5
Cornic Mathieu <mathieu.cornic@soprasteria.com>
2015-11-27 16:42:53 +0100

    Clean: cleaning code and refactored code in utils functions

--- common/websocket/websocket.go
+++ common/websocket/websocket.go
@@ -2,14 +2,15 @@ package websocket
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
-	"github.com/gin-gonic/gin"
 	"github.com/fsamin/intools-engine/common/logs"
+	"github.com/fsamin/intools-engine/common/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 var (
 	appclient = &AppClient{
-		Clients:make(map[*websocket.Conn]*Client),
+		Clients: make(map[*websocket.Conn]*Client),
 	}
 	wsupgrader = websocket.Upgrader{
 		ReadBufferSize:  1024,
@@ -18,47 +19,44 @@ var (
 )
 
 type Client struct {
-	Socket			*websocket.Conn
-	GroupIds 	[]string
+	Socket   *websocket.Conn
+	GroupIds []string
 }
 
 type AppClient struct {
-	Clients	map[*websocket.Conn]*Client
+	Clients map[*websocket.Conn]*Client
 }
 
-type Message struct{
-
-	Command				string						`json:"Command"`
-	Data			map[string]interface{}		`json:"data"`
-
+type Message struct {
+	Command string                 `json:"Command"`
+	Data    map[string]interface{} `json:"data"`
 }
 
 // Registers the connection from to the intools engine
 func (appClient *AppClient) Register(conn *websocket.Conn) {
-
 	// Add the client to the connected clients
 	logs.Debug.Printf("Connection event from client")
 
 	var client = &Client{
-		Socket: conn,
+		Socket:   conn,
 		GroupIds: []string{},
 	}
-	logs.Debug.Printf("clients before %v \n", appClient.Clients)
+	logs.Debug.Printf("clients before %v", appClient.Clients)
 
 	appClient.Clients[conn] = client
 	// appClient.Clients = append(appClient.Clients, client)
 
-	logs.Debug.Printf("clients %v \n", appClient.Clients)
+	logs.Debug.Printf("clients %v", appClient.Clients)
 
 	// Send ack
 	message := Message{
-		Command:	"connected",
-		Data:	nil,
+		Command: "connected",
+		Data:    nil,
 	}
 	conn.WriteJSON(message)
 
 	// Handles events from client
-	Events:
+Events:
 	for {
 		var message Message
 		err := conn.ReadJSON(&message)
@@ -84,33 +82,32 @@ func (appClient *AppClient) Register(conn *websocket.Conn) {
 			logs.Debug.Printf("Registered group %s for client %p", message.Data["groupId"], client)
 		case "unregister-group":
 			// Handles group unregistering for the client
-			client.GroupIds = del(client.GroupIds, message.Data["groupId"].(string))
-			logs.Debug.Printf("Unregistered group %s for client %p", message.Data["groupId"], client)
+			i := utils.IndexOf(client.GroupIds, message.Data["groupId"].(string))
+			if i != -1 {
+				client.GroupIds = append(client.GroupIds[:i], client.GroupIds[i+1:]...)
+				logs.Debug.Printf("Unregistered group %s for client %p", message.Data["groupId"], client)
+			}
 		}
-
 		logs.Debug.Printf("Registered groups for client %p are now : %s", client, client.GroupIds)
-
 	}
-
 }
 
 // Broadcasts the value to all client registred to the group
 func Notify(groupId string, value *map[string]interface{}) {
-
 	logs.Debug.Printf("Notify all client registred to groupid %s , with value %v", groupId, value)
 
 	logs.Debug.Printf("clients %v \n", appclient.Clients)
 
-	for _,client := range appclient.Clients {
+	for _, client := range appclient.Clients {
 
-		if contains(client.GroupIds, groupId) {
+		if utils.Contains(client.GroupIds, groupId) {
 			data := map[string]interface{}{
-			"groupId" : groupId,
-			"value" : value,
+				"groupId": groupId,
+				"value":   value,
 			}
 			message := Message{
-				Command:	"connector-value",
-				Data:	data,
+				Command: "connector-value",
+				Data:    data,
 			}
 			logs.Debug.Printf("Notifying client %p with message %s", client, message)
 			client.Socket.WriteJSON(message)
@@ -119,33 +116,12 @@ func Notify(groupId string, value *map[string]interface{}) {
 }
 
 // Get websocket
-func GetWS(c *gin.Context){
-
+func GetWS(c *gin.Context) {
 	conn, err := wsupgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		fmt.Println("Failed to set websocket upgrade: %+v", err)
 		return
 	}
-
 	// Registering the connection from Intools-front
 	appclient.Register(conn)
-}
-
-func contains(slice []string, item string) bool {
-	set := make(map[string]struct{}, len(slice))
-	for _, s := range slice {
-		set[s] = struct{}{}
-	}
-	_, ok := set[item]
-	return ok
-}
-
-func del(slice[]string, item string) []string {
-	var r[]string
-	for _, str := range slice {
-		if str != item {
-			r = append(r, str)
-		}
-	}
-	return r
 }
